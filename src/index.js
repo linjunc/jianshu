012815b024b20a4191ab203488231eeac090c51a
linjunc <ljc15113180751@163.com>
2021-09-12 14:00:52 +0800

    push first data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+/*
+ * @Author: 林俊丞
+ * @Date: 2021-09-12 13:53:04
+ * @LastEditors: 林俊丞
+ * @LastEditTime: 2021-09-12 13:55:28
+ * @Description: 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
